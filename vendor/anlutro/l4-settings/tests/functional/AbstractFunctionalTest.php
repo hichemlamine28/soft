dcacde2b029e2a9409293d6327a06e7404336776
hichemlamine <hicemlamine@gmail.com>
2021-02-05 22:16:26 +0100

    add files

---
+++ vendor/anlutro/l4-settings/tests/functional/AbstractFunctionalTest.php
@@ -0,0 +1,144 @@
+<?php
+
+use anlutro\LaravelSettings\DatabaseSettingStore;
+use Illuminate\Support\Facades\Config;
+use PHPUnit\Framework\TestCase;
+
+abstract class AbstractFunctionalTest extends TestCase
+{
+	protected abstract function createStore(array $data = array());
+
+	public function setUp(): void
+	{
+		Config::shouldReceive('get')
+			->with('settings.enableCache')
+			->andReturn(false)
+			->getMock();
+
+		Config::shouldReceive('get')
+			->with('settings.forgetCacheByWrite')
+			->andReturn(false)
+			->getMock();
+
+		Config::shouldReceive('get')
+			->with('settings.defaults.foo')
+			->andReturn(false)
+			->getMock();
+
+		Config::shouldReceive('get')
+			->with('settings.defaults.bar')
+			->andReturn(false)
+			->getMock();
+	}
+
+	protected function assertStoreEquals($store, $expected, $message = '')
+	{
+		$this->assertEquals($expected, $store->all(), $message);
+		$store->save();
+		$store = $this->createStore();
+		$this->assertEquals($expected, $store->all(), $message);
+	}
+
+	protected function assertStoreKeyEquals($store, $key, $expected, $message = '')
+	{
+		$this->assertEquals($expected, $store->get($key), $message);
+		$store->save();
+		$store = $this->createStore();
+		$this->assertEquals($expected, $store->get($key), $message);
+	}
+
+	/** @test */
+	public function store_is_initially_empty()
+	{
+		$store = $this->createStore();
+		$this->assertEquals(array(), $store->all());
+	}
+
+	/** @test */
+	public function written_changes_are_saved()
+	{
+		$store = $this->createStore();
+		$store->set('foo', 'bar');
+		$this->assertStoreKeyEquals($store, 'foo', 'bar');
+	}
+
+	/** @test */
+	public function nested_keys_are_nested()
+	{
+		$store = $this->createStore();
+		$store->set('foo.bar', 'baz');
+		$this->assertStoreEquals($store, array('foo' => array('bar' => 'baz')));
+	}
+
+	/** @test */
+	public function cannot_set_nested_key_on_non_array_member()
+	{
+		$store = $this->createStore();
+		$store->set('foo', 'bar');
+		$this->expectException('UnexpectedValueException');
+		$this->expectExceptionMessage('Non-array segment encountered');
+		$store->set('foo.bar', 'baz');
+	}
+
+	/** @test */
+	public function can_forget_key()
+	{
+		$store = $this->createStore();
+		$store->set('foo', 'bar');
+		$store->set('bar', 'baz');
+		$this->assertStoreEquals($store, array('foo' => 'bar', 'bar' => 'baz'));
+		
+		$store->forget('foo');
+		$this->assertStoreEquals($store, array('bar' => 'baz'));
+	}
+
+	/** @test */
+	public function can_forget_nested_key()
+	{
+		$store = $this->createStore();
+		$store->set('foo.bar', 'baz');
+		$store->set('foo.baz', 'bar');
+		$store->set('bar.foo', 'baz');
+		$this->assertStoreEquals($store, array(
+			'foo' => array(
+				'bar' => 'baz',
+				'baz' => 'bar',
+			),
+			'bar' => array(
+				'foo' => 'baz',
+			),
+		));
+		
+		$store->forget('foo.bar');
+		$this->assertStoreEquals($store, array(
+			'foo' => array(
+				'baz' => 'bar',
+			),
+			'bar' => array(
+				'foo' => 'baz',
+			),
+		));
+
+		$store->forget('bar.foo');
+		$expected = array(
+			'foo' => array(
+				'baz' => 'bar',
+			),
+			'bar' => array(
+			),
+		);
+		if ($store instanceof DatabaseSettingStore) {
+			unset($expected['bar']);
+		}
+		$this->assertStoreEquals($store, $expected);
+	}
+
+	/** @test */
+	public function can_forget_all()
+	{
+		$store = $this->createStore(array('foo' => 'bar'));
+		$this->assertStoreEquals($store, array('foo' => 'bar'));
+		$store->forgetAll();
+		$this->assertStoreEquals($store, array());
+	}
+}
